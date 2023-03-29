277829b1d4929e07007dc165b9645f152985af1c
hieumai943 <hieumai09042003@gmail.com>
2023-03-29 17:41:29 +0700

    success to add file

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-let storageKey ='text';
-let saveFile = ['hieuuuuuuuuuuu']
- document.querySelector('#submit').addEventListener('click',() =>{
- 
-   
-        let data= CKEDITOR.instances.content.getData();
-        localStorage.setItem('editorData',data);
+let storageKey = "text";
+var savedData = localStorage.getItem(storageKey);
 
-    console.log('submitted');
-    localStorage.setItem(storageKey,saveFile);
+// Set the saved data to the editor instance
+var editor = CKEDITOR.instances.content;
+editor.setData(savedData);
+let data;
+document.querySelector("#submit").addEventListener("click", () => {
+  data = CKEDITOR.instances.content.getData();
+  if(data.length==0){
+        alert('Không thể tạo file do chưa có dữ liệu');
+  }
+  localStorage.setItem(storageKey, data);
+  editor.setData('');
+//   dang can nhac doan nay khong biet co nen remove item hay khong
+  localStorage.removeItem(storageKey);
+  
+});
+
+document.querySelector('.storageFile').addEventListener("click",()=>{
+        editor.setData(data)
 })
+function showList(){
+        document.querySelector('.storageFile').innerText = 'FILE 1';
+}
+
+
