3d9b1de06482e02e781de0d09c66a99c4e7b6c4e
hieumai943 <hieumai09042003@gmail.com>
2023-03-30 01:45:50 +0700

    possible to save file and open file by localstorage

--- main.js
+++ main.js
@@ -6,35 +6,37 @@ var editor = CKEDITOR.instances.content;
 editor.setData(savedData);
 let data;
 let para;
-let output='';
+
 document.querySelector("#submit").addEventListener("click", () => {
         count++;
+        localStorage.setItem('numFile', count);
         data = CKEDITOR.instances.content.getData();
   if(data.length==0){
         alert('Không thể tạo file do chưa có dữ liệu');
   }
   else{
   localStorage.setItem(`storageKey${count}`, data);
-//   editor.setData('');
+  editor.setData('');
 //   dang can nhac doan nay khong biet co nen remove item hay khong
-//   localStorage.removeItem(storageKey);
+
   
   showList(count);
  
   }
 });
-
+showList(localStorage.getItem('numFile'));
 
 
 function showList(numFile){
-       
-        output+=`<div class="file" style="display: flex" onclick="completed(${numFile})">File ${numFile}</div>`
+        let output='';
+        for(let i=1;i<= numFile;i++){
+        output+=`<div class="file" style="display: flex" onclick="completed(${i})">File ${i}</div>`
         document.querySelector('.storageFile').innerHTML= output;
+        }
         
 }
 
 function completed(num){
-        
        editor.setData(localStorage.getItem(`storageKey${num}`))
 }
 
