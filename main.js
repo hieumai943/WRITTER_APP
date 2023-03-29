d77f8714a5a0845666761e27feb5c324ebadf43f
hieumai943 <hieumai09042003@gmail.com>
2023-03-30 02:38:13 +0700

    fix some bug and and decorate little bit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let storageKey = "text";
 var savedData = localStorage.getItem(storageKey);
-let count =0;
+let count = 0;
 // Set the saved data to the editor instance
 var editor = CKEDITOR.instances.content;
 editor.setData(savedData);
@@ -8,35 +8,29 @@ let data;
 let para;
 
 document.querySelector("#submit").addEventListener("click", () => {
-        count++;
-        localStorage.setItem('numFile', count);
-        data = CKEDITOR.instances.content.getData();
-  if(data.length==0){
-        alert('Không thể tạo file do chưa có dữ liệu');
-  }
-  else{
-  localStorage.setItem(`storageKey${count}`, data);
-  editor.setData('');
-//   dang can nhac doan nay khong biet co nen remove item hay khong
+  localStorage.setItem("numFile", count);
+  data = CKEDITOR.instances.content.getData();
+  if (data.length == 0) {
+    alert("Không thể tạo file do chưa có dữ liệu");
+  } else {
+    count++;
+    localStorage.setItem(`storageKey${count}`, data);
+    editor.setData("");
+    //   dang can nhac doan nay khong biet co nen remove item hay khong
 
-  
-  showList(count);
- 
+    showList(count);
   }
 });
-showList(localStorage.getItem('numFile'));
+showList(localStorage.getItem("numFile"));
 
-
-function showList(numFile){
-        let output='';
-        for(let i=1;i<= numFile;i++){
-        output+=`<div class="file" style="display: flex" onclick="completed(${i})">File ${i}</div>`
-        document.querySelector('.storageFile').innerHTML= output;
-        }
-        
+function showList(numFile) {
+  let output = "";
+  for (let i = 1; i <= numFile; i++) {
+    output += `<span class="file"  onclick="completed(${i})"><i class="fa fa-file-o" aria-hidden="true" style="margin-right:10px"></i>File ${i}</span>`;
+    document.querySelector(".storageFile").innerHTML = output;
+  }
 }
 
-function completed(num){
-       editor.setData(localStorage.getItem(`storageKey${num}`))
+function completed(num) {
+  editor.setData(localStorage.getItem(`storageKey${num}`));
 }
-
