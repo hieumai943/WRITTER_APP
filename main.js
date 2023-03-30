24faaf9944b56e974d83d481db201aba70f43012
hieumai943 <hieumai09042003@gmail.com>
2023-03-30 16:37:14 +0700

    finish save file and delete file

--- main.js
+++ main.js
@@ -1,54 +1,53 @@
-let storageKey = "text";
+
 var savedData = localStorage.getItem(storageKey);
 let count = 0;
-
 // Set the saved data to the editor instance
 var editor = CKEDITOR.instances.content;
 editor.setData(savedData);
 let data;
 let para;
-
+let tasklist=[];
 document.querySelector("#submit").addEventListener("click", () => {
  
+  
+  
+   console.log(tasklist);
   data = CKEDITOR.instances.content.getData();
   if (data.length == 0) {
     alert("Không thể tạo file do chưa có dữ liệu");
   } else {
+    
     count++;
     localStorage.setItem("numFile", count);
-    localStorage.setItem(`storageKey${count}`, data);
+    tasklist.push(data);
+    localStorage.setItem('storageKey',JSON.stringify(tasklist) );
     editor.setData("");
     //   dang can nhac doan nay khong biet co nen remove item hay khong
     console.log(data);
-    showList(count);
+    showList();
   }
+
 });
-showList(localStorage.getItem("numFile"));
 
-function showList(numFile) {
+
+function showList() {
+  let localItems = JSON.parse(localStorage.getItem('storageKey'));
+  if(localItems=== null ) tasklist=[];
+  else tasklist= localItems;
   let output = "";
-  for (let i = 1; i <= numFile; i++) {
-    if(localStorage.getItem(`storageKey${i}`)!= null){
-    output += `<span class="file"  onclick="completed(${i})"><i class="fa fa-file-o" aria-hidden="true" style="margin-right:10px"></i>File ${i}<i class="fa fa-trash-o" aria-hidden="true" style='margin-left:30px' onclick="deleteFile(${i})"></i></span>`;
-    document.querySelector(".storageFile").innerHTML = output;
-  }
+  
+  tasklist.forEach((data,index)=>{
+    output += `<span class="file"  onclick="completed(${index})"><i class="fa fa-file-o" aria-hidden="true" style="margin-right:10px"></i>${tasklist[index]}<i class="fa fa-trash-o" aria-hidden="true" style='margin-left:30px' onclick="deleteFile(${index})"></i></span>`;
+  })
+  document.querySelector(".storageFile").innerHTML = output;
 }
-}
-
+showList();
 function completed(num) {
-  editor.setData(localStorage.getItem(`storageKey${num}`));
+  editor.setData(tasklist[num]);
 }
 
 function deleteFile(num){
-  localStorage.removeItem(`storageKey${num}`);
-
-  localStorage.setItem("numFile", localStorage.getItem('numFile')-1);
-  let output = "";
-  document.querySelector(".storageFile").innerHTML = output;
-  for (let i = 1; i <= count; i++) {
-    if(localStorage.getItem(`storageKey${i}`)!= null){
-    output += `<span class="file"  onclick="completed(${i})"><i class="fa fa-file-o" aria-hidden="true" style="margin-right:10px"></i>File ${i}<i class="fa fa-trash-o" aria-hidden="true" style='margin-left:30px' onclick="deleteFile(${i})"></i></span>`;
-    document.querySelector(".storageFile").innerHTML = output;
-  }
-}
+tasklist.splice(num,1);
+localStorage.setItem('storageKey', JSON.stringify(tasklist));
+showList();
 }
