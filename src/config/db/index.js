45680ca7da6dafc652529eb3c97a19afb12321c1
hieumai943 <hieumai09042003@gmail.com>
2023-05-19 23:16:02 +0700

    connect to mongodb atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 async function connect(){
     try{
+        mongoose.connect('mongodb+srv://hieumai090403:fxYRdhinfwa6MHvZ@deploy-vercel.pmbhwxj.mongodb.net/?retryWrites=true&w=majority')
+        .then(()=>{
+            console.log('connect success to mongodb atlas');
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
         await mongoose.connect('mongodb://127.0.0.1:27017/f8_learning_dev',{
             useNewUrlParser: true,
             useUnifiedTopology: true
